3496c877a35b96cb378adc4fcdabba626a59ba1a
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-22 20:32:44 +0800

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -70,49 +70,6 @@ chmod +x ohpserver
 cp ohpserver /usr/local/bin/ohpserver
 /bin/rm -rf ohpserver*
 
-# Installing Service
-# SSH OHP Port 8181
-cat > /etc/systemd/system/ssh-ohp.service << END
-[Unit]
-Description=SSH OHP Redirection Service
-Documentation=https://t.me/Hendra2012
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/local/bin/ohpserver -port 8181 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:22
-Restart=on-failure
-LimitNOFILE=infinity
-
-[Install]
-WantedBy=multi-user.target
-END
-
-# Dropbear OHP 8282
-cat > /etc/systemd/system/dropbear-ohp.service << END
-[Unit]]
-Description=Dropbear OHP Redirection Service
-Documentation=https://t.me/Hendra2012
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/local/bin/ohpserver -port 8282 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:109
-Restart=on-failure
-LimitNOFILE=infinity
-
-[Install]
-WantedBy=multi-user.target
-END
-
 # OpenVPN OHP 8383
 cat > /etc/systemd/system/openvpn-ohp.service << END
 [Unit]]
@@ -134,11 +91,6 @@ LimitNOFILE=infinity
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl enable ssh-ohp
-systemctl restart ssh-ohp
-systemctl enable dropbear-ohp
-systemctl restart dropbear-ohp
 systemctl enable openvpn-ohp
 systemctl restart openvpn-ohp
 #------------------------------
@@ -149,16 +101,6 @@ if [ -n "$(ss -tupln | grep ohpserver | grep -w 8181)" ]
 then
 	echo 'SSH OHP Redirection Running'
 else
-	echo 'SSH OHP Redirection Not Found, please check manually'
-fi
-sleep 0.5
-if [ -n "$(ss -tupln | grep ohpserver | grep -w 8282)" ]
-then
-	echo 'Dropbear OHP Redirection Running'
-else
-	echo 'Dropbear OHP Redirection Not Found, please check manually'
-fi
-sleep 0.5
 if [ -n "$(ss -tupln | grep ohpserver | grep -w 8383)" ]
 then
 	echo 'OpenVPN OHP Redirection Running'
