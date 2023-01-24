ab630b8ec1df3eb6eb38888ecd32d96093641f92
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-24 15:07:58 +0800

    Update m-update.sh

--- menu/m-update.sh
+++ menu/m-update.sh
@@ -25,29 +25,73 @@ clear
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-echo -e " [INFO] Downloading Update File"
-sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu.sh" && chmod +x /usr/bin/menu
-#wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vmess.sh" && chmod +x /usr/bin/m-vmess
-wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vless.sh" && chmod +x /usr/bin/m-vless
-wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-trojan.sh" && chmod +x /usr/bin/m-trojan
-wget -q -O /usr/bin/m-system "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-system.sh" && chmod +x /usr/bin/m-system
-wget -q -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
-wget -q -O /usr/bin/m-sshws "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-ssws.sh" && chmod +x /usr/bin/m-ssws
-wget -q -O /usr/bin/m-webmin "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-webmin.sh" && chmod +x /usr/bin/m-webmin
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/m-domain "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-domain.sh" && chmod +x /usr/bin/m-domain
-wget -q -O /usr/bin/bw "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/bw.sh" && chmod +x /usr/bin/bw
-#wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/restart.sh" && chmod +x /usr/bin/autoboot
-wget -q -O /usr/bin/m-tcp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-tcp.sh" && chmod +x /usr/bin/m-tcp
-wget -q -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
-wget -q -O /usr/bin/clearcache "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/clearcache.sh" && chmod +x /usr/bin/clearcache
-#wget -q -O /usr/bin/info "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/update.sh" && chmod +x /usr/bin/update
-#wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
-#wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/restart.sh" && chmod +x /usr/bin/restart
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-update.sh" && chmod +x /usr/bin/m-update
-echo -e " [INFO] Update Successfully"
+echo -e "\E[44;1;39m ⇱ [INFO] Downloading Update File⇲        \E[0m"
 sleep 5
-exit
+echo -e "\E[44;1;39m               ⇱ 5 ⇲                      \E[0m"
+sleep 4
+echo -e "\E[44;1;39m               ⇱ 4 ⇲                      \E[0m"
+sleep 3
+echo -e "\E[44;1;39m               ⇱ 3 ⇲                      \E[0m"
+sleep 2
+echo -e "\E[44;1;39m               ⇱ 2 ⇲                      \E[0m"
+sleep 1
+echo -e "\E[44;1;39m               ⇱ 1 ⇲                      \E[0m"
+echo -e""
+echo -e "\033[0;36m==================================================\033[m"
+echo -e "\E[44;1;39m              ⇱ UPDATE ⇲                      \E[0m"
+echo -e "\E[44;1;39m              ⇱ DIMULAI ⇲                    \E[0m"
+echo -e "\033[0;36m==================================================\033[m"
+# hapus menu
+rm -rf menu
+rm -rf m-vmess
+rm -rf m-vless
+rm -rf m-trojan
+rm -rf m-system
+rm -rf m-sshovpn
+rm -rf m-ssws
+rm -rf m-webmin
+rm -rf running
+rm -rf m-domain
+rm -rf bw
+rm -rf m-tcp
+rm -rf auto-reboot
+rm -rf clearcache
+rm -rf restart
+
+# download menu
+cd /usr/bin
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/bin/m-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vmess.sh" && chmod +x /usr/bin/m-vmess
+wget -O /usr/bin/m-vless "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vless.sh" && chmod +x /usr/bin/m-vless
+wget -O /usr/bin/m-trojan "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-trojan.sh" && chmod +x /usr/bin/m-trojan
+wget -O /usr/bin/m-system "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-system.sh" && chmod +x /usr/bin/m-system
+wget -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
+wget-O /usr/bin/m-sshws "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-ssws.sh" && chmod +x /usr/bin/m-ssws
+wget -O /usr/bin/m-webmin "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-webmin.sh" && chmod +x /usr/bin/m-webmin
+wget -O /usr/bin/running "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/running.sh" && chmod +x /usr/bin/running
+wget -O /usr/bin/m-domain "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-domain.sh" && chmod +x /usr/bin/m-domain
+wget -O /usr/bin/bw "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/bw.sh" && chmod +x /usr/bin/bw
+wget -O /usr/bin/m-tcp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-tcp.sh" && chmod +x /usr/bin/m-tcp
+wget -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
+wget -O /usr/bin/clearcache "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/clearcache.sh" && chmod +x /usr/bin/clearcache
+wget -O /usr/bin/restart "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/restart.sh" && chmod +x /usr/bin/restart
+wget -O /usr/bin/update "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-update.sh" && chmod +x /usr/bin/m-update
+
+chmod +x menu
+chmod +x m-vmess
+chmod +x m-vless
+chmod +x m-trojan
+chmod +x m-system
+chmod +x m-sshovpn
+chmod +x m-ssws
+chmod +x m-webmin
+chmod +x running
+chmod +x m-domain
+chmod +x bw
+chmod +x m-tcp
+chmod +x auto-reboot
+chmod +x clearcache
+chmod +x restart
+"\E[44;1;39m ⇱ [INFO] Update Successfully & SELESAI....... ⇲    \E[0m"
+sleep 5
+cd
