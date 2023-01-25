1db766f3da13ff43faebf893ddef80293bc02e9f
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-25 11:56:59 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -384,7 +384,7 @@ wget -O m-tcp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/tc
 
 wget -O xp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/ssh/xp.sh"
 #wget -O asu "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/asu.sh"
-wget -O sl-fix "https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/sslh-fix/sl-fix"
+wget -O sl-fix "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/sslh-fix"
 
 chmod +x sl-fix
 chmod +x menu
